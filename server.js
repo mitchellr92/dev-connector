9be1c5593797a5e6180330dd011a6c70203b6bf7
Mitchell Robles <mitchellr92@live.com>
2020-01-23 14:24:36 -0800

    api routes for users, auth, profiles and posts

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -7,6 +7,12 @@ const app = express();
 connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
 
 const PORT = process.env.PORT || 5000;
 
