de9e2807981962d2b2cdc735fb4a4c4785579cf5
Mitchell Robles <mitchellr92@live.com>
2020-01-24 11:02:16 -0800

    Get request for all profiles and for profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -26,7 +26,7 @@ router.get("/me", auth, async (req, res) => {
   }
 });
 
-// @route   GET api/profile
+// @route   POST api/profile
 // @desc    Create or update user profile
 // @access  Private
 router.post(
@@ -109,5 +109,40 @@ router.post(
     res.send("Hello");
   }
 );
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile)
+      return res.status(400).json({ msg: "Profile not found" });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
