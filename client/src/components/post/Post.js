535453bd64cfa26b4c80b03db8c755558b5645a9
Mitchell Robles <mitchellr92@live.com>
2020-02-05 07:29:36 -0800

    Adding comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
+import CommentForm from "./CommentForm";
 import { getPost } from "../../actions/post";
 
-const Post = ({ getPost, post: {post, loading}, match }) => {
-
+const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
   }, [getPost, match.params.id]);
@@ -20,6 +20,7 @@ const Post = ({ getPost, post: {post, loading}, match }) => {
         Back To Posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
     </Fragment>
   );
 };
