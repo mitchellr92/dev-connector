41eb727a8f3c26c9a967d872aeb080d8c7c419ba
Mitchell Robles <mitchellr92@live.com>
2020-02-06 18:15:00 -0800

    Trying to fix deployment issues

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -11,6 +11,7 @@ const ProfileTop = ({
     user: { name, avatar }
   }
 }) => {
+  console.log(website);
   return (
     <div className="profile-top bg-primary p-2">
       <img className="round-img my-1" src={avatar} alt="" />
@@ -25,27 +26,27 @@ const ProfileTop = ({
             <i className="fas fa-globe fa-2x" />
           </a>
         )}
-        {social && social.twitter && (
+        {social.twitter && (
           <a href={social.twitter} target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitter fa-2x" />
           </a>
         )}
-        {social && social.facebook && (
+        {social.facebook && (
           <a href={social.facebook} target="_blank" rel="noopener noreferrer">
             <i className="fab fa-facebook fa-2x" />
           </a>
         )}
-        {social && social.linkedin && (
+        {social.linkedin && (
           <a href={social.linkedin} target="_blank" rel="noopener noreferrer">
             <i className="fab fa-linkedin fa-2x" />
           </a>
         )}
-        {social && social.youtube && (
+        {social.youtube && (
           <a href={social.youtube} target="_blank" rel="noopener noreferrer">
             <i className="fab fa-youtube fa-2x" />
           </a>
         )}
-        {social && social.instagram && (
+        {social.instagram && (
           <a href={social.instagram} target="_blank" rel="noopener noreferrer">
             <i className="fab fa-instagram fa-2x" />
           </a>
